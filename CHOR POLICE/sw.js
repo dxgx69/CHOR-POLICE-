3d171caab19778cfbe39a6ff853a7d193da078a7
dxgx69 <dxgx1978@gmail.com>
2026-04-06 11:57:10 +0530

    Add files via upload

---
+++ CHOR POLICE/sw.js
@@ -0,0 +1,12 @@
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  // Simple fetch without caching to prevent login stickiness
+  e.respondWith(fetch(e.request));
+});
